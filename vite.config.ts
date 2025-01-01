0967edfbacf77e6ff6e0bf7518651150251107d2
Luis <luiz7@umes.edu.gt>
2024-12-31 21:04:39 -0600

    31Dic

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    jsxFactory: '_jsx',
+    jsxFragment: '_jsxFragment',
+    jsxInject: `import { createElement as _jsx,
+    Fragment as _jsxFragment } from 'react'`,
+  },
   plugins: [react()],
 })
