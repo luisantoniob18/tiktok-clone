da4894ecf4e383d595bae0ca8cdbca693df587a5
Luis <luiz7@umes.edu.gt>
2025-01-02 13:43:07 -0600

    feat(Root): Start working on Videos Actions Sidebar

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -1,33 +1,43 @@
 import { useEffect, useRef, useState } from 'react'
 import styles from './styles.module.css'
+import clsx from 'clsx'
+import VideoPlayerActions from './VideoPlayerActions'
 
-const SRC = 'https://p16-sign-va.tiktokcdn.com/tos-maliva-p-0068/oEI6QABxRd3PAfDnpFxcfMD86qkEbE5QJRTTBA~tplv-photomode-zoomcover:720:720.avif?lk3s=b59d6b55&x-expires=1735862400&x-signature=rZ6BsuETcRIvwta%2B%2BgYOQQbq5GE%3D&shp=b59d6b55&shcp=-'
-
-export default function VideoPlayer(){
+export default function VideoPlayer({src}){
     const [playing, setPlaying] = useState(false)
-    const video = useRef()
+    const video = useRef(null)
+
     const handlePlay = () => {
-        if(playing){
-            video.current.pause()
-        }else{
-            video.current.play()
-        }
+        //const {current: videoEl} = video
+        const videoEl = video.current;
+
+        playing ? 
+        videoEl.pause()
+        : videoEl.play()
         
         setPlaying(!playing)
     }
+
     useEffect(() => {
 
     }, [])
 
+    const playerClassName = clsx(styles.player, {
+        [styles.hidden]: playing,
+    })
+
     return(
-        <div>
+        <div className={styles.wrapper}>
             <video
         className={styles.video} 
-        src={SRC} 
+        src={src}
+        loop 
         controls={false}
         ref={video}
+        onClick={handlePlay}
          />
-         <button className={styles.player} onClick={handlePlay}/>
+         <i className={playerClassName} onClick={handlePlay}/>
+         <VideoPlayerActions/>
         </div>
         
     )
