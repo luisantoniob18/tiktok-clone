623cca7a8bc190b318e51794b793cce89cf72681
Luis <luiz7@umes.edu.gt>
2025-01-02 13:43:52 -0600

    feat(Root): Use FeedVideos component to show more than one video

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import VideoPlayer from './components/VideoPlayer'
+import FeedVideos from './components/FeedVideos/index.jsx'
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className='App'>
       <main>
-        <VideoPlayer/>
+        <FeedVideos />
       </main>      
     </div>
   )
